269aef5f4083111ebea2e52d4f6f8ca66ebd0527
wl <13992787306@163.com>
2025-01-24 22:15:59 +0800

    1.添加保存按钮;

--- merchant/merchant-front/src/views/sales/SalesOrderSelect.vue
+++ merchant/merchant-front/src/views/sales/SalesOrderSelect.vue
@@ -1,68 +1,78 @@
 <template>
-  <div class="frame-page flex flex-column">
-    <vxe-toolbar>
-      <template #buttons>
-      </template>
-      <template #tools>
-        <Select v-model="params.state" class="w-120px" :datas="{已保存:'未审核',已审核:'已审核'}"
-                placeholder="审核状态："/>
-        <div class="h-input-group">
-          <span class="h-input-addon ml-8px">订单日期：</span>
-          <DateRangePicker v-model="dateRange"></DateRangePicker>
-        </div>
-        <div class="h-input-group">
-          <span class="h-input-addon ml-8px">客户：</span>
-          <Select class="w-178px" filterable :datas="customerList" keyName="id" titleName="name"
-                  v-model="params.customerId" placeholder="请选择客户"  />
-        </div>
-        <Search v-model.trim="params.filter" search-button-theme="h-btn-default"
-                show-search-button class="w-360px ml-8px"
-                placeholder="请输入订单号" @search="doSearch">
-          <i class="h-icon-search"/>
-        </Search>
-      </template>
-    </vxe-toolbar>
-    <div class="flex1">
-      <vxe-table row-id="id"
-                 ref="table"
-
-                 :data="dataList"
-                 highlight-hover-row
-                 show-overflow
-                 show-footer
-                 :footer-method="footerMethod"
-                 :row-config="{height: 48}"
-                 :column-config="{resizable: true}"
-                 :sort-config="{remote:true}"
-                 :loading="loading">
-        <vxe-column type="checkbox" width="40" align="center"/>
-        <vxe-column title="操作" align="center" width="120">
-          <template #default="{row}">
-            <span class="primary-color  text-hover ml-10px" @click="addForm('edit',row.id)">编辑</span>
-            <span class="primary-color  text-hover ml-10px" @click="doRemove(row)">删除</span>
-          </template>
-        </vxe-column>
-        <vxe-column title="订单日期" field="orderDate" align="center" width="130"/>
-        <vxe-column title="订单编号" field="orderNo" width="200"/>
-        <vxe-column title="关联销售出库单" field="code" width="200"/>
-        <vxe-column title="客户" field="customerName" min-width="120"/>
-        <vxe-column title="销售金额" field="totalAmount" width="120"/>
-        <vxe-column title="折扣金额" field="discountAmount" width="120"/>
-        <vxe-column title="折后金额" field="finalAmount" width="120"/>
-      </vxe-table>
-    </div>
-    <div class="flex justify-between items-center pt-5px">
-      <vxe-pager perfect @page-change="loadList(false)"
-                 v-model:current-page="pagination.page"
-                 v-model:page-size="pagination.pageSize"
-                 :total="pagination.total"
-                 :layouts="['PrevJump', 'PrevPage', 'Number', 'NextPage', 'NextJump', 'Sizes', 'Total']">
-        <template #left>
-          <span class="mr-12px text-16px">总金额：{{ amountTotal }}元</span>
-          <vxe-button @click="loadList(false)" type="text" size="mini" icon="h-icon-refresh"
-                      :loading="loading"></vxe-button>
+  <div class="modal-column">
+    <div class="modal-column-full-body">
+      <vxe-toolbar>
+        <template #buttons>
         </template>
-      </vxe-pager>
+        <template #tools>
+          <Select v-model="params.state" class="w-120px" :datas="{已保存:'未审核',已审核:'已审核'}"
+                  placeholder="审核状态："/>
+          <div class="h-input-group">
+            <span class="h-input-addon ml-8px">订单日期：</span>
+            <DateRangePicker v-model="dateRange"></DateRangePicker>
+          </div>
+          <div class="h-input-group">
+            <span class="h-input-addon ml-8px">客户：</span>
+            <Select class="w-178px" filterable :datas="customerList" keyName="id" titleName="name"
+                    v-model="params.customerId" placeholder="请选择客户"  />
+          </div>
+          <Search v-model.trim="params.filter" search-button-theme="h-btn-default"
+                  show-search-button class="w-360px ml-8px"
+                  placeholder="请输入订单号" @search="doSearch">
+            <i class="h-icon-search"/>
+          </Search>
+        </template>
+      </vxe-toolbar>
+      <div class="flex1">
+        <vxe-table row-id="id"
+                   ref="table"
+
+                   :data="dataList"
+                   highlight-hover-row
+                   show-overflow
+                   show-footer
+                   :footer-method="footerMethod"
+                   :row-config="{height: 48}"
+                   :column-config="{resizable: true}"
+                   :sort-config="{remote:true}"
+                   :loading="loading">
+          <vxe-column type="checkbox" width="40" align="center"/>
+          <vxe-column title="操作" align="center" width="120">
+            <template #default="{row}">
+              <span class="primary-color  text-hover ml-10px" @click="addForm('edit',row.id)">编辑</span>
+              <span class="primary-color  text-hover ml-10px" @click="doRemove(row)">删除</span>
+            </template>
+          </vxe-column>
+          <vxe-column title="订单日期" field="orderDate" align="center" width="130"/>
+          <vxe-column title="订单编号" field="orderNo" width="200"/>
+          <vxe-column title="关联销售出库单" field="code" width="200"/>
+          <vxe-column title="客户" field="customerName" min-width="120"/>
+          <vxe-column title="销售金额" field="totalAmount" width="120"/>
+          <vxe-column title="折扣金额" field="discountAmount" width="120"/>
+          <vxe-column title="折后金额" field="finalAmount" width="120"/>
+        </vxe-table>
+      </div>
+      <div class="flex justify-between items-center pt-5px">
+        <vxe-pager perfect @page-change="loadList(false)"
+                   v-model:current-page="pagination.page"
+                   v-model:page-size="pagination.pageSize"
+                   :total="pagination.total"
+                   :layouts="['PrevJump', 'PrevPage', 'Number', 'NextPage', 'NextJump', 'Sizes', 'Total']">
+          <template #left>
+            <span class="mr-12px text-16px">总金额：{{ amountTotal }}元</span>
+            <vxe-button @click="loadList(false)" type="text" size="mini" icon="h-icon-refresh"
+                        :loading="loading"></vxe-button>
+          </template>
+        </vxe-pager>
+      </div>
+    </div>
+    <div class="modal-column-between">
+      <Button @click="$emit('close')" :loading="loading">
+        取消
+      </Button>
+      <Button color="primary" @click="confirm" :loading="loading">
+        保存
+      </Button>
     </div>
   </div>
 </template>
@@ -71,10 +81,7 @@ import manba from "manba";
 import SalesOrder from "@js/api/sales/SalesOrder";
 import {mapMutations} from "vuex";
 import {confirm, loading, message} from "heyui.ext";
-import PurchaseOrder from "@js/api/purchase/PurchaseOrder";
 import Customer from "@js/api/basic/Customer";
-import Warehouse from "@js/api/basic/Warehouse";
-import Product from "@js/api/basic/Product";
 
 const startTime = manba().startOf(manba.MONTH).format("YYYY-MM-dd");
 const endTime = manba().endOf(manba.DAY).format("YYYY-MM-dd");

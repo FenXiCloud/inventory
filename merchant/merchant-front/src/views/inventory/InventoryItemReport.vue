c537b2bd85cfa2df8805acc7fb3db622b9e8ccc8
chase <1246408474@qq.com>
2025-03-21 02:53:06 +0800

    fix: 调整明细来往单位条件

--- merchant/merchant-front/src/views/inventory/InventoryItemReport.vue
+++ merchant/merchant-front/src/views/inventory/InventoryItemReport.vue
@@ -58,10 +58,10 @@
         <vxe-column title="单据编号" field="batchNumber" width="200"/>
         <vxe-column title="往来单位" field="supplierName" width="120">
           <template #default="{ row }">
-            <div v-if="inboundItems.includes(row['operationType'])">
+            <div v-if="['其他入库','盘盈入库'].includes(row['operationType'])">
               {{ row.supplierName }}
             </div>
-            <div v-else-if="outboundItems.includes(row['operationType'])">
+            <div v-else-if="['其他出库','盘亏出库'].includes(row['operationType'])">
               {{ row.customerName }}
             </div>
             <div v-else>

1aa52c090e2d4194ed78aecb7970ee2722937a07
shuaiqi <1691536530@qq.com>
2025-05-19 17:42:35 +0800

    启动问题修复

--- merchant/merchant-front/src/views/basic/AccountList.vue
+++ merchant/merchant-front/src/views/basic/AccountList.vue
@@ -45,7 +45,7 @@
 <script>
 import Account from "@js/api/basic/Account";
 import AccountForm from "./AccountForm.vue";
-import AccountFlowReport from "./AccountFlowList.vue";
+import AccountFlowReport from "../fund/AccountFlowList.vue";
 import {confirm, message} from "heyui.ext";
 import {layer} from "@layui/layer-vue";
 import {h} from "vue";
